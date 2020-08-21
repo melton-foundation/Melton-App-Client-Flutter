e5825580051f195c67058790f212f3879e60d9a5
Pranav Bijapur <bijapurpranav@gmail.com>
2020-08-22 00:13:10 +0530

    feat(directory): update to use correct filter icon

--- lib/screens/directory.dart
+++ lib/screens/directory.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:melton_app/constants/constants.dart' as Constants;
 import 'package:melton_app/api/api.dart';
 import 'package:melton_app/models/UserModel.dart';
@@ -104,7 +105,7 @@ class _DirectoryState extends State<Directory> {
         IconButton(
           onPressed: () {},
           icon: Icon(
-            Icons.filter_list,
+            FontAwesomeIcons.filter,
             color: Colors.white,
             size: 25,
           ),
