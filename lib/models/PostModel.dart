de5633f2768659f01be2990b53e1b1281893baa2
Pranav Bijapur <bijapurpranav@gmail.com>
2020-08-24 00:46:56 +0530

    feat(posts): preview image

--- lib/models/PostModel.dart
+++ lib/models/PostModel.dart
@@ -3,20 +3,22 @@ class PostModel {
   String title;
   String description;
   String content;
+  String previewImage;
   List<String> tags;
   DateTime created;
   DateTime lastUpdated;
 
-  PostModel({this.id, this.title, this.description, this.content, this.tags, this.created,
-      this.lastUpdated});
+  PostModel({this.id, this.title, this.description, this.content, this.previewImage,
+    this.tags, this.created, this.lastUpdated});
 
   factory PostModel.fromJson(Map<String, dynamic> responsePost) {
     return PostModel(id: responsePost['id'], title: responsePost['title'],
-      description: responsePost['description'], tags: new List<String>.from(responsePost['tags']),
-      content: responsePost['content'],
-      created: DateTime.parse(responsePost['created']),
-      // lastUpdated is null if post hasn't been updated
-      lastUpdated: responsePost['created'].split(".")[0] == responsePost['updated'].split(".")[0] ?
+        description: responsePost['description'], tags: new List<String>.from(responsePost['tags']),
+        content: responsePost['content'],
+        previewImage: responsePost['preview'],
+        created: DateTime.parse(responsePost['created']),
+        // lastUpdated is null if post hasn't been updated
+        lastUpdated: responsePost['created'].split(".")[0] == responsePost['updated'].split(".")[0] ?
         null : DateTime.parse(responsePost['updated']));
   }
 }
