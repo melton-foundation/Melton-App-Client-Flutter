2e98c6232979cb3ba1200e9b44aaa9e9c825fbdb
akash933 <ak.asp.com@gmail.com>
2020-10-08 21:48:45 +0530

    feat(sentry): Sentry integration to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:melton_app/constants/constants.dart';
-
 import 'package:melton_app/screens/splash.dart';
+import 'package:melton_app/util/secrets.dart';
 import 'package:melton_app/util/service_locator.dart';
+import 'package:sentry/sentry.dart';
+
+final sentry = SentryClient(dsn: Secrets.SENTRY_CLIENT_DSN);
 
 // todo optimize imports in all files
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await setupLocator();
-  runApp(MyApp());
+  runZonedGuarded(
+        () => runApp(MyApp()),
+        (error, stackTrace) async {
+      await sentry.captureException(
+        exception: error,
+        stackTrace: stackTrace,
+      );
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
