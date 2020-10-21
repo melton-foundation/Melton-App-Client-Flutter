507cb05443e13375dc39536135cc4c7a0911a85c
Pranav Bijapur <bijapurpranav@gmail.com>
2020-10-21 12:43:04 +0530

    chore(test): run dartfmt on new files

--- test/api/mock_sentry_service.dart
+++ test/api/mock_sentry_service.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:melton_app/sentry/SentryService.dart';
 
 class MockSentryService extends SentryService {
-
   bool isSentryErrorReported = false;
 
   @override
-  Future<void> reportErrorToSentry({@required dynamic error, dynamic stackTrace}) async {
+  Future<void> reportErrorToSentry(
+      {@required dynamic error, dynamic stackTrace}) async {
     isSentryErrorReported = true;
     return;
   }
